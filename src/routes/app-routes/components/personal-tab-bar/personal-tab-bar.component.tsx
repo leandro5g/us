8ffb75040d0752dcc841348b47d8fbe2101dd167
Leandro Moraes <leandromoraes12345@outlook.com>
2022-10-14 10:45:06 -0300

    :sparkles: add config hooks eslint

--- src/routes/app-routes/components/personal-tab-bar/personal-tab-bar.component.tsx
+++ src/routes/app-routes/components/personal-tab-bar/personal-tab-bar.component.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import React from 'react';
+import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
 
-import { Container } from "./personal-tab-bar.styles";
+import { Container } from './personal-tab-bar.styles';
 
 type PersonalTabBarProps = BottomTabBarProps;
 
 const PersonalTabBar: React.FC<PersonalTabBarProps> = ({ insets }) => {
-  return <Container />;
+    return <Container />;
 };
 
 export { PersonalTabBar };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
-import { AppRoutes } from "./app-routes";
-import { AuthRoutes } from "./auth-routes/auth.routes";
+import { AppRoutes } from './app-routes';
+import { AuthRoutes } from './auth-routes/auth.routes';
 
 const Routes: React.FC = () => {
-  return (
-    <NavigationContainer>
-      <AuthRoutes />
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <AuthRoutes />
+        </NavigationContainer>
+    );
 };
 
 export { Routes };
